faa0677ccaa546091406a2b27996c3b3da1a4e8f
KidMongoose <kidmongoose@gmail.com>
2023-05-04 21:45:56 +0700

    Initial commit

---
+++ app.rb
@@ -0,0 +1,35 @@
+import sinatra
+
+
+configure 
+
+end
+
+get '/' do
+    
+end
+
+
+get '/login' do
+    
+end
+
+get '/logout' do
+    
+end
+
+
+get '/post/:id' do
+    
+end
+
+get '/posts' do
+    
+end
+
+
+get '/dashboard' do
+    
+end
+
+
